8fb2f3c0c6715c217d29ef8404948ad2ab672b9e
Gabriela Menezes <gabimenezess10@gmail.com>
2020-06-12 15:40:59 -0300

    transitions adicionadas

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
-// menu hamburguer
+//menu hamburguer
 const $menuBtn = document.querySelector('.icon-menu');
 const $menuList = document.querySelector('.menu');
 const $content = document.querySelector('.content');
 
 
+/*ACIONANDO O MENU BURGUER*/
 $menuBtn.addEventListener('click', function() {
     $menuList.classList.toggle('menu_move');
     $content.classList.toggle('content_move')
 })
+
+
